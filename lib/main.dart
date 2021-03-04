9b5128c7bbe4e7edd0448a5fb4ea0b8baeeddbb4
Czerwcuu <czerwiecr1999@gmail.com>
2021-03-04 19:19:15 +0100

    Profile created

--- lib/main.dart
+++ lib/main.dart
@@ -11,37 +11,62 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.green[500],
         body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Container(
-                height: double.infinity,
-                width: 100,
-                color: Colors.red,
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('images/test.png'),
               ),
-              Container(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Container(
-                      width: 100,
-                      height: 100,
-                      color: Colors.yellow,
-                    ),
-                    Container(
-                      width: 100,
-                      height: 100,
-                      color: Colors.limeAccent[700],
-                    ),
-                  ],
+              Text(
+                'Radosław Czerwiec',
+                style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Roboto'),
+              ),
+              Text('FLUTTER DEVELOPER',
+                  style: TextStyle(fontSize: 12, fontFamily: 'Gothic')),
+              SizedBox(
+                height: 20.0,
+                child: Divider(),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Padding(
+                  padding: EdgeInsets.all(12.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(Icons.mail),
+                      SizedBox(
+                        width: 25.0,
+                      ),
+                      Text('czerwiecr1999@gmail.com',
+                          style: TextStyle(fontWeight: FontWeight.bold))
+                    ],
+                  ),
                 ),
               ),
-              Container(
-                height: double.infinity,
-                width: 100,
-                color: Colors.blue,
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.call,
+                    color: Colors.black,
+                  ),
+                  title: Text(
+                    '+48 000 000 000',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                child: Divider(),
               ),
             ],
           ),
